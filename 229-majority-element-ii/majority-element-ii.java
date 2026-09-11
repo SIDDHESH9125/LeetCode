8be5d2e305e4a4bd723a2ff4cfbb30fbe9141b4a
Siddhesh Patil <siddheshpatil5912@gmail.com>
2026-09-11 16:25:51 +0530

    Time: 3 ms (73.51%) | Memory: 52.5 MB (64.97%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,18 +1,37 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int n= nums.length /3;
-        List<Integer> m= new ArrayList<>();
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int num : nums){
-            map.put(num,map.getOrDefault(num,0)+1);
+       int n=nums.length;
+       int cnt1=0 , cnt2=0;
+       int el1=Integer.MIN_VALUE , el2=Integer.MIN_VALUE;
+
+       for(int i=0;i<n;i++){
+        if(cnt1==0 && nums[i]!=el2){
+            cnt1++;
+            el1=nums[i];
+        }else if(cnt2==0 && nums[i]!=el1){
+            cnt2++;
+            el2=nums[i];
+        }else if(nums[i]==el1){
+            cnt1++;
+        }else if(nums[i]==el2){
+            cnt2++;
+        }else{
+            cnt1--;
+            cnt2--;
+        }
+       }
+        cnt1=0;cnt2=0;
+
+        for(int j=0;j<n;j++){
+            if(nums[j]==el1) cnt1++;
+            if(nums[j]==el2)cnt2++;
         }
 
-        for(int i=0;i<nums.length;i++){
-            if(map.get(nums[i])>n){
-                if(!m.contains(nums[i])){
-                  m.add(nums[i]);
-                }
-            }
-        }return m;
+        int m=n/3+1;
+        ArrayList<Integer> res= new ArrayList<>();
+        if(cnt1>=m) res.add(el1);
+        if(cnt2>=m && el2!=el1) res.add(el2);
+       
+       return res;
     }
 }
