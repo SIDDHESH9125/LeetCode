e88418677f4ef9179f883786441629cef083cb6e
Siddhesh Patil <siddheshpatil5912@gmail.com>
2026-09-26 16:41:51 +0530

    Time: 4 ms (56.52%) | Memory: 43.2 MB (91.93%) - LeetSync

---
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        int depth=0;
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='('){
+                if(depth > 0){
+                    sb.append('(');
+                    depth++;
+                }else{
+                    depth++;
+                }
+            }else if(s.charAt(i)==')'){
+                if(depth>1){
+                    sb.append(')');
+                    depth--;
+                }else{
+                    depth--;
+                }
+            }
+            
+        }return sb.toString();
+    }}
